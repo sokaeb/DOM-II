6f69c324d2d4db0b97fd6ca39d2e1dfca60f298c
Kae Benton <sonja.k.benton@gmail.com>
2020-08-04 15:30:37 -0400

    Wheel event applied.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+                            // MOUSEOVER event listener applied to H1
 const pageHeading = document.querySelector('h1');
 pageHeading.addEventListener('mouseover', function(event){
     event.target.style.color = 'aqua';
@@ -14,7 +16,7 @@ navLink1 = navLinks[0];
 navLink2 = navLinks[1];
 navLink3 = navLinks[2];
 navLink4 = navLinks[3];
-
+                            // MOUSEOVER event listener applied to all <a> 
 navLink1.addEventListener('mouseover', function(event){
     event.target.style.color = 'red';
 
@@ -46,3 +48,14 @@ navLink4.addEventListener('mouseover', function(event){
         event.target.style.color = "";
     }, 400)
 }, false)
+
+// WHEEL event listener applied to header image
+const busImg = document.querySelector('.intro img')
+let scale = 1;
+busImg.addEventListener('wheel', grow);
+
+function grow(event){
+    scale += event.deltaY * -0.01;
+    scale = Math.min(Math.max(.125, scale), 4);
+    busImg.style.transform = `scale(${scale})`;
+};
